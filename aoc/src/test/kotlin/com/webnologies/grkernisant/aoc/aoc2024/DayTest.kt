d4be03474ce28cca5d42541d4050646a93a2018e
Guy-Robert Kernisant <grkernisant@gmail.com>
2026-06-05 20:07:24 +0200

    feat: add env_var support

--- aoc/src/test/kotlin/com/webnologies/grkernisant/aoc/aoc2024/DayTest.kt
+++ aoc/src/test/kotlin/com/webnologies/grkernisant/aoc/aoc2024/DayTest.kt
@@ -1,8 +1,26 @@
 package com.webnologies.grkernisant.aoc.aoc2024
 
 interface DayTest {
+    fun getInputName(): String {
+        return this::class.simpleName?.lowercase()?.replaceFirstChar { it.uppercase() } ?: throw IllegalStateException("Anonymous class")
+    }
+
+    fun readEnv(): Map<String, String> {
+        val name = getInputName()
+        val envFile = object {}.javaClass.getResourceAsStream("/2024/$name.env")
+            ?.bufferedReader()
+            ?.readLines()
+            ?: emptyList()
+
+        return envFile.fold(mutableMapOf<String, String>()) { acc, curr ->
+            val matches = ENV_REGEX.matchEntire(curr)
+            if (matches != null) acc[matches.groupValues[1]] = matches.groupValues[2]
+            acc
+        }
+    }
+
     fun readInput(): List<String> {
-        val name = this::class.simpleName?.lowercase()?.replaceFirstChar { it.uppercase() } ?: throw IllegalStateException("Anonymous class")
+        val name = getInputName()
         return object {}.javaClass.getResourceAsStream("/2024/$name.txt")
             ?.bufferedReader()
             ?.readLines()
